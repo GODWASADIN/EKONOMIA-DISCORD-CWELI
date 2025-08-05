a99117464cc4d058d21f769ecf82bef6dfe86955
GODWASADIN <kacper.szypura3@gmail.com>
2025-08-05 19:00:06 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
-from economy import get_user_data
+from economy import load_data, save_data, update_reputation, get_user_data
+from discord.ext import commands, tasks
+import random
 import discord
-from discord.ext import commands
 import os
-
+import time
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
 @bot.event
@@ -20,5 +21,45 @@ async def bal(ctx):
     embed.add_field(name="🏦 Bank", value=f"{user['bank']} $")
     embed.add_field(name="⭐ Reputacja", value=f"{user['reputation']} pkt")
     await ctx.send(embed=embed)
+
+@bot.command()
+async def work(ctx):
+    if ctx.channel.name != 'ekonomia':
+        return await ctx.send("Komenda działa tylko na kanale #ekonomia!")
+
+    user_id = str(ctx.author.id)
+    current_time = time.time()
+    cooldown = 1800  # 30 minut cooldownu
+
+    # sprawdzenie cooldownu
+    if user_id in cooldowns and current_time - cooldowns[user_id] < cooldown:
+        remaining = int((cooldown - (current_time - cooldowns[user_id])) / 60)
+        return await ctx.send(f"⏳ Musisz poczekać jeszcze {remaining} min, by ponownie pracować!")
+
+    earnings = random.randint(20, 100)
+
+    # Sprawdzenie bonusu reputacji
+    user = get_user_data(user_id)
+    if user['reputation'] >= 50:
+        earnings = int(earnings * 1.2)
+        bonus_msg = " (+20% bonus za reputację!)"
+    else:
+        bonus_msg = ""
+
+    data = load_data()
+    user_data = data.setdefault(user_id, {'cash': 0, 'bank': 0, 'reputation': 0})
+    user_data['cash'] += earnings
+    save_data(data)
+
+    update_reputation(user_id, 6)  # +6 reputacji za pracę
+    cooldowns[user_id] = current_time  # ustawienie cooldownu
+
+    embed = discord.Embed(
+        title="💼 Praca zakończona!",
+        description=f"Zarobiłeś **{earnings}$**{bonus_msg}\nTwoja reputacja wzrosła o **+6 pkt**!",
+        color=discord.Color.green()
+    )
+    await ctx.send(embed=embed)
+    
 
 bot.run(os.getenv('DISCORD_TOKEN'))
