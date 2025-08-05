2bffac7a2ec18737c5de97ad3e8afe191773476b
GODWASADIN <kacper.szypura3@gmail.com>
2025-08-05 18:52:07 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,24 @@
+from economy import get_user_data
 import discord
 from discord.ext import commands
-import os  # <--- brakujący import
+import os
 
-bot = commands.Bot(command_prefix='/', intents=discord.Intents.all())
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
     print(f'Zalogowano jako {bot.user}')
+
+@bot.command()
+async def stan(ctx):
+    if ctx.channel.name != 'ekonomia':
+        return await ctx.send("Komenda działa tylko na kanale #ekonomia!")
+
+    user = get_user_data(ctx.author.id)
+    embed = discord.Embed(title=f"bal {ctx.author.name}", color=discord.Color.green())
+    embed.add_field(name="💵 Gotówka", value=f"{user['cash']} $")
+    embed.add_field(name="🏦 Bank", value=f"{user['bank']} $")
+    embed.add_field(name="⭐ Reputacja", value=f"{user['reputation']} pkt")
+    await ctx.send(embed=embed)
 
 bot.run(os.getenv('DISCORD_TOKEN'))
